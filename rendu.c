a57e356c6bcf9712b625a23a038af4baaf15c8de
Florian <44389189+Florian-Fogliani@users.noreply.github.com>
2024-12-16 21:35:22 +0100

    Update rendu.c

--- rendu.c
+++ rendu.c
@@ -1,25 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static unsigned int combo_rec(const unsigned int* values, unsigned count, unsigned int amount, unsigned index)
+void coin_combo_rec(const unsigned int* values, unsigned count, unsigned int amount, unsigned int act, unsigned int *dp)
 {
-    if (amount == 0)
-        return 1;
-    unsigned int res = 0;
-    for(unsigned i = index; i < count; i++)
+    if (act >= count)
     {
-        res += combo_rec(values, count, amount - values[index], i);
+        return;
     }
+        int coin = values[act];
+        for (int i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+        coin_combo_rec(values, count, amount, act + 1, dp);
+    }
+
+
+unsigned int
+coin_combo(const unsigned int* values, unsigned count, unsigned int amount)
+{
+    unsigned int *dp = malloc(sizeof(unsigned int)*(amount+1));
+    for (int i = 0; i <= amount; i++) {
+        dp[i] = 0;
+    }
+    dp[0] = 1;
+    coin_combo_rec(values,count, amount, 0, dp);
+    unsigned int res = dp[amount];
+    free(dp);
     return res;
-}
-
-unsigned int coin_combo(const unsigned int* values, unsigned count, unsigned int amount)
-{
-    return combo_rec(values, count, amount, 0);
-}
-
-int main(void)
-{
-    unsigned coins[] = { 3, 7, 10 };
-    printf("%u", coin_combo(coins, 3, 31));
 }
